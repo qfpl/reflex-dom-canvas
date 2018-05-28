67fd55adb07ccac8be42627df581a550a70d6381
Sean Chalmers <sclhiannan@gmail.com>
2018-05-28 10:59:33 +1000

    Cleaned up main default.nix to prevent examples from being built when you only want the library. Added nix expressions to examples so that it's buildable as a separate thing.

--- default.nix
+++ default.nix
@@ -11,39 +11,9 @@ let
     });
   };
 
-  adjust-for-ghcjs = drv: {
-    executableToolDepends = [pkgs.closurecompiler pkgs.zopfli];
-    doHaddock = false;
-    postInstall = ''
-      mkdir -p $out
+  basics = pkgs.haskell.lib.overrideCabal (
+    haskellPackages.callPackage ./reflex-dom-canvas.nix {}
+  ) {};
 
-      mkdir -p $out/js
-      cp $out/bin/reflex-dom-canvas.jsexe/all.js $out/js/reflex-dom-canvas.js
-
-      mkdir -p $out/css
-      ln -s ./css/* $out/css
-
-      ln -s $out/bin/reflex-dom-canvas.jsexe/index.html $out/index.html
-
-      cd $out/bin/reflex-dom-canvas.jsexe
-      closure-compiler all.js --compilation_level=ADVANCED_OPTIMIZATIONS --isolation_mode=IIFE --assume_function_wrapper --jscomp_off="*" --externs=all.js.externs > $out/js/reflex-dom-canvas.min.js
-      rm -Rf $out/bin/reflex-dom-canvas.jsexe
-      rm -Rf $out/bin
-
-      cd $out/js
-      zopfli -i1000 reflex-dom-canvas.min.js
-
-      rm -Rf $out/lib
-      rm -Rf $out/nix-support
-      rm -Rf $out/share
-    '';
-  };
-
-  adjust = drv:
-    if compiler == "ghcjs"
-    then adjust-for-ghcjs drv
-    else drv;
-
-  basics = pkgs.haskell.lib.overrideCabal (haskellPackages.callPackage ./reflex-dom-canvas.nix {}) adjust;
 in
   basics
