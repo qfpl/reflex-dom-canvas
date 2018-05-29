2e640ae5b759ea8732d098a6ef4bec701dc42bda
Sean Chalmers <sclhiannan@gmail.com>
2018-05-29 10:47:24 +1000

    Deactivate haddocks for GHCJS as there is an error during compilation

--- default.nix
+++ default.nix
@@ -4,6 +4,10 @@
 let
   pkgs = reflex-platform.nixpkgs.pkgs;
 
+  adjust-for-ghcjs = drv: {
+    doHaddock = false;
+  };
+
   haskellPackages = reflex-platform.${compiler}.override {
     overrides = (self: super: {
       ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
@@ -11,9 +15,14 @@ let
     });
   };
 
+  adjust = drv:
+    if compiler == "ghcjs"
+    then adjust-for-ghcjs drv
+    else drv;
+
   basics = pkgs.haskell.lib.overrideCabal (
     haskellPackages.callPackage ./reflex-dom-canvas.nix {}
-  ) {};
+  ) adjust;
 
 in
   basics
